8320770a96408af25e24025205e49b327556af1f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-04-11 14:29:09 +0200

    Types and interfaces part1

--- app.ts
+++ app.ts
@@ -1,11 +1,54 @@
-class User {
-  constructor(private name: string) {}
+let coffeeSize: "medium" | "large";
 
-  sayHello(): string {
-    return this.name;
+coffeeSize = "medium";
+coffeeSize = "small";
+
+let stringOrNumber: string | number;
+stringOrNumber = 255;
+stringOrNumber = "max";
+
+type doubleAnything = string | number
+
+function double(value: doubleAnything): number {
+  if(typeof value === "string") {
+    return  Number(value) * 2;
   }
+
+  return value * 2;
 }
 
-const user = new User('Adam');
+type maybeValue = string | undefined;
 
-user.sayHello();
+let valueFromDOM;
+
+function show(value: maybeValue): maybeValue {
+  return value;
+}
+
+show(valueFromDOM);
+
+interface Knife {
+  slice(): string;
+}
+
+interface Scissors {
+  cut(): string;
+}
+
+type multiTool = Knife & Scissors;
+
+function useTool(tool: multiTool): void {
+  tool.cut();
+  tool.slice();
+}
+
+const swissArmyKnife: multiTool = {
+  slice() {
+    return 'slice!'
+  },
+  cut() {
+    return 'cut!'
+  },
+}
+
+useTool(swissArmyKnife);
